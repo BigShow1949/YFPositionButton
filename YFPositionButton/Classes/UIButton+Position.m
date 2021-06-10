44c317f77644a13c39d3e4cf18586d6b5c33081d
BigShow1949 <1029883589@qq.com>
2021-06-10 10:58:55 +0800

    image width&height

--- YFPositionButton/Classes/UIButton+Position.m
+++ YFPositionButton/Classes/UIButton+Position.m
@@ -28,18 +28,23 @@
     
     
     // 1. 得到imageView和titleLabel的宽、高
-    CGFloat imageWith = self.imageView.frame.size.width;
-    CGFloat imageHeight = self.imageView.frame.size.height;
+
     
-    CGFloat labelWidth = 0.0;
+    CGFloat labelWidth  = 0.0;
     CGFloat labelHeight = 0.0;
+    CGFloat imageWith   = 0.0;
+    CGFloat imageHeight = 0.0;
     if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
         // 由于iOS8中titleLabel的size为0，用下面的这种设置
         labelWidth = self.titleLabel.intrinsicContentSize.width;
         labelHeight = self.titleLabel.intrinsicContentSize.height;
+        imageWith = self.imageView.intrinsicContentSize.width;
+        imageHeight = self.imageView.intrinsicContentSize.height;
     } else {
         labelWidth = self.titleLabel.frame.size.width;
         labelHeight = self.titleLabel.frame.size.height;
+        imageWith = self.imageView.intrinsicContentSize.width;
+        imageHeight = self.imageView.intrinsicContentSize.height;
     }
     
     // 2. 声明全局的imageEdgeInsets和labelEdgeInsets
